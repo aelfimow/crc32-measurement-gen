d3bd15aff48dcab97e726e36887a8f6b2a924171
Alexander Elfimow <my.free.mail@gmx.de>
2020-06-04 14:16:22 +0200

    generate: generate full function

--- src/generate.cpp
+++ src/generate.cpp
@@ -10,17 +10,20 @@ struct param_regs
     r64 &crc_reg;
     r64 &p_data_reg;
     r64 &count_reg;
+    r64 &tmp_reg;
 };
 
-static void gen_function(param_regs &pr, r64 &out_reg)
+static void gen_function(param_regs &pr, r64 &out_reg, size_t value)
 {
     std::string const func_name { "crc32_8" };
 
-    comment("uint32_t " + func_name + "(uint32_t crc, const void *p_data, size_t count)");
+    comment("uint32_t " + func_name + "(uint32_t crc_iv, const void *p_data, size_t count)");
 
-    comment("crc is in " + pr.crc_reg.name());
+    comment("crc_iv is in " + pr.crc_reg.name());
     comment("p_data is in " + pr.p_data_reg.name());
     comment("count is in " + pr.count_reg.name());
+    comment("output crc is in " + out_reg.name());
+    comment("temporary register is " + pr.tmp_reg.name());
 
     global(func_name);
 
@@ -29,10 +32,26 @@ static void gen_function(param_regs &pr, r64 &out_reg)
 
     label(func_name);
 
+    MOV(out_reg, pr.crc_reg);
+
+    imm64 inc_value { value };
+    MOV(pr.tmp_reg, inc_value);
+
+    std::string func_start { func_name + "_start" };
+    std::string func_end { func_name + "_end" };
+
+    label(func_start);
+    TEST(pr.count_reg, pr.count_reg);
+    JZ(func_end);
+
     m8 buf_addr { pr.p_data_reg };
 
     CRC32(out_reg, buf_addr);
+    ADD(pr.p_data_reg, pr.tmp_reg);
+    DEC(pr.count_reg);
+    JMP(func_start);
 
+    label(func_end);
     RET();
 }
 
@@ -60,24 +79,24 @@ try
     {
         param_regs windows_param_regs
         {
-            RCX, RDX, R8
+            RCX, RDX, R8, R9
         };
 
         r64 &out_reg { RAX };
 
-        gen_function(windows_param_regs, out_reg);
+        gen_function(windows_param_regs, out_reg, 1);
     }
 
     if (for_linux)
     {
         param_regs linux_param_regs
         {
-            RDI, RSI, RDX
+            RDI, RSI, RDX, RCX
         };
 
         r64 &out_reg { RAX };
 
-        gen_function(linux_param_regs, out_reg);
+        gen_function(linux_param_regs, out_reg, 1);
     }
 
     return EXIT_SUCCESS;
